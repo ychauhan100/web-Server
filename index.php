9707120708036cedd3fbfd1cc9cdb8466bab83ee
ychauhan100 <64644582+ychauhan100@users.noreply.github.com>
2023-01-24 20:42:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-This is Microsoft Azure Server  <br><br><br><?php echo gethostname(); // may output e.g,: sandie?>" > index.php
+This is AWS Server  <br><br><br><?php echo gethostname(); // may output e.g,: sandie?>" > index.php
